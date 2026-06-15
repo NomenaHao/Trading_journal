b88cbc097bb0037ae68762f627962dfbb5ce3cf7
Nomena <leehaoming6@gmail.com>
2026-06-15 14:57:16 +0300

    Favicon

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import logo from '../assets/logo.png'
 import viteLogo from '../assets/vite.svg'
 import heroImg from '../assets/hero.png'
 import vueLogo from '../assets/vue.svg'
